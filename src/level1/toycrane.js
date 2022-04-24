3a99d4889d023d125ec17ec4d601c23c45052426
Dahye <06robin11@gmail.com>
2022-04-24 21:17:03 +0900

    codetest edited

--- src/level1/toycrane.js
+++ src/level1/toycrane.js
@@ -1,27 +1,31 @@
 //unsolved
 function solution(board, moves){
-    let basket = [];
+    let basket = [0];
+    let result =0;
     const newMoves = moves.map((item)=>item-1);
     for(let move of newMoves){
         const toyrow = board.find(item=> item[move] !== 0);
         if(toyrow !== undefined) {
             basket.push(toyrow[move])
             toyrow[move] = 0;
-        }
-    }
-    console.log(basket);
-    let result =0;
-    function removeToy(){
-        for(let i=1; i<=basket.length ; i++){
-            if(basket[i] === basket[i-1]){
-                result +=2
-                basket = basket.filter(item=>item != basket[i]);
-                //console.log(basket);
-                removeToy();
-            }
-        }
-    }
-    removeToy();
+            
+           // if(basket[-1] === basket[-2]) basket.pop(); basket.pop();
+            result +=2
+         //   break;
+      }  }
+     
+      console.log(basket);
+    // function removeToy(){
+    //     for(let i=1; i<=basket.length ; i++){
+    //         if(basket[i] === basket[i-1]){
+    //             result +=2
+    //             basket = basket.filter(item=>item != basket[i]);
+    //             //console.log(basket);
+    //             removeToy();
+    //         }
+    //     }
+    // }
+    // removeToy();
     console.log(result);
     return result;
 }
