fbf2dd4b83055d64f58b559e7ea21f63791b8afe
Dahye <06robin11@gmail.com>
2022-09-21 13:51:57 +0900

    string problem

--- src/level1/sizerCode-unsolved.js
+++ src/level1/sizerCode-unsolved.js
@@ -1,27 +1,21 @@
-//시저암호
-
-function sizerCode(str,num){
-    let answer = '';
-    for(let i =0 ; i<str.length ; i++){
-    let charCode = str.charCodeAt(i);
-        if(str[i] == ' '){
-            answer += ' ';
-        }
-        else if(str[i] == 'z'){
-            answer += String.fromCharCode(97+num-1);
-        }
-        else if(str[i] == 'Z'){
-            answer += String.fromCharCode(65+num-1);
-        }
-        else{
-            if(charCode+num > 122){
-            answer += String.fromCharCode(charCode+num-26)}
-            else{
-            answer += String.fromCharCode(charCode+num)
-            };
-    }   
-    }
-    return answer;
+function sizerCode(str, num) {
+  return str
+    .split("")
+    .map((each) => {
+      if (each === " ") return " ";
+      const unicode = each.charCodeAt(0);
+      const newUnicdoe = unicode + num;
+      if (unicode < 91) {
+        return String.fromCharCode(
+          newUnicdoe > 90 ? newUnicdoe - 26 : newUnicdoe
+        );
+      } else {
+        return String.fromCharCode(
+          newUnicdoe > 122 ? newUnicdoe - 26 : newUnicdoe
+        );
+      }
+    })
+    .join("");
 }
 
-console.log(sizerCode('a   eZLo',25));
+console.log(sizerCode("a   eZLo", 25));
